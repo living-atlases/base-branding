691f751d31bd398bbc9db5fe3ef2d5f80a2900b0
vjrj <vjrj@comunes.org>
2024-05-29 10:23:44 +0200

    Workaround to load css from styles

--- app/js/init.js
+++ app/js/init.js
@@ -6,6 +6,29 @@ import './stats.js';
 import './autocomplete-conf.js';
 import './import-css.js';
 
+async function loadThemeResources(theme) {
+  try {
+    // Importar CSS del tema
+    // await import(`../themes/${theme}/css/styles.css`);
+    if (theme == "material") {
+      await import('../themes/material/css/material-custom-styles.css');
+      await import('../themes/material/css/cas-style.css');
+    } else {
+      await import('../themes/clean/css/lang.css');
+      await import('../themes/clean/css/cas-style.css');
+      await import('../themes/clean/css/sticky-footer.css');
+      if (theme != 'clean') {
+        await import(`../themes/${theme}/css/bootstrap.min.css`);
+      }
+    }
+
+    console.log(`Resources for theme ${theme} loaded successfully.`);
+  } catch (error) {
+    console.error(`Failed to load resources for theme ${theme}:`, error);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('LA skin initialized');
+  loadThemeResources(settings.theme);
 });
